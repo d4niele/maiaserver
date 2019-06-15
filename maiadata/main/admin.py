a6c160c9c6feea5a57216737276092bd1d99fb5e
Daniele <daniele.rizzo@spacespa.it>
2019-06-15 09:45:19 +0200

    _

--- maiadata/main/admin.py
+++ maiadata/main/admin.py
@@ -6,5 +6,5 @@ from .models import Data
 class AuthorAdmin(admin.ModelAdmin):
     def has_add_permission(self, request, obj=None):
         return False
-    
+    list_filter = ('espid','topic')
     list_display = ('espid','timereg', 'topic','timestamp','peso','temperatura','umidita')
