1917886983e2d857bd63ec15194b7cbe417379fa
Your Name <you@example.com>
2019-06-17 17:39:12 +0200

    _

--- maiadata/main/views.py
+++ maiadata/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import *
 
-def charts(request):
-    records = Data.objects.order_by('timereg').filter(espid='Ascea')[:5000]
+def charts(request,espid="Ascea"):
+    records = Data.objects.order_by('timereg').filter(espid=espid)
     return render(request, 'charts.html', {'records':records})
